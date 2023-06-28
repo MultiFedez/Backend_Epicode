dd00b5636a6a0e87f0c5d2e3accca9eebdb1feda
Federico Morgante <federicomorgante88@gmail.com>
2023-06-28 19:02:03 +0200

    Esercizio g3

--- Lezione3_g3/src/Esercizio4_g3/Esercizio4_Main.java
+++ Lezione3_g3/src/Esercizio4_g3/Esercizio4_Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Esercizio4_Main {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		
+		contoAllaRovescia();
 	}
 	
 	public static void contoAllaRovescia() {
 		
-		System.out.println("conto alla rovescia");
+		System.out.println("conto alla rovescia:");
 		int partenza = Integer.parseInt(sc.nextLine());
 		for(int i = partenza; i >= 0; i--) {
 			System.out.println(i);
