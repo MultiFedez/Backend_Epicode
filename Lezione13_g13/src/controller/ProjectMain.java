22ae7249203703211452d54bc4737e48a110c566
Federico Morgante <federicomorgante88@gmail.com>
2023-07-13 20:00:44 +0200

    Esercizio g13

---
+++ Lezione13_g13/src/controller/ProjectMain.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.time.LocalDate;
+
+import enumeration.TipoEvento;
+import model.Evento;
+import model.EventoDAO;
+
+public class ProjectMain {
+
+	public static void main(String[] args) {
+		
+		Evento e = new Evento("Calici di stelle",LocalDate.of(2023,11,21),"bere vino",TipoEvento.PUBBLICO,100);
+		Evento e2 = new Evento("ferragosto",LocalDate.of(2023,8,15), "festa in spiaggia",TipoEvento.PUBBLICO,800);
+		
+	
+		EventoDAO eventoDao = new EventoDAO();
+		eventoDao.save(e);
+		eventoDao.getById(2);
+		eventoDao.save(e2);
+		eventoDao.delete(e2);
+		eventoDao.refresh(e2);
+		
+	
+
+	}
+
+}
